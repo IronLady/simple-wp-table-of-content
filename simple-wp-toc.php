d32404d7af0789b6b5bf1d29683d42576f4abe01
Samuel Sena <samuelsena88@gmail.com>
2026-04-10 11:14:27 +0700

    fix: bump version

--- simple-wp-toc.php
+++ simple-wp-toc.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Simple WP Table of Contents Block
  * Description:       Adds a dynamic table of contents block with block-sidebar controls for core Heading blocks.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Samuel Sena
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SIMPLE_WP_TOC_VERSION', '1.0.0' );
+define( 'SIMPLE_WP_TOC_VERSION', '1.0.1' );
 define( 'SIMPLE_WP_TOC_FILE', __FILE__ );
 define( 'SIMPLE_WP_TOC_URL', plugin_dir_url( __FILE__ ) );
 define( 'SIMPLE_WP_TOC_PATH', plugin_dir_path( __FILE__ ) );
